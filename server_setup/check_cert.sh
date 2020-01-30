79c148ae78d4f779d99c7856d21b0911a2c1db2a
rendezvous <47820025+rendezvous@users.noreply.github.com>
2020-01-30 19:11:06 +0100

    Add files via upload
    First version of the server scripts (mosquitto server and certificates + certificate renewal)

---
+++ server_setup/check_cert.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+cd /etc/mosquitto/certs
+expiration_date=$(openssl x509 -text -noout -in server.crt | grep "Not After")
+expiration_epoch=$(date -d "${expiration_date//Not After : /}" +"%s")
+
+current_date=$(date)
+current_epoch=$(date -d "$currentdate" +"%s")
+
+seconds_left=$(( $expiration_epoch - $current_epoch ))
+days_left=$(( $seconds_left / ( 3600 * 24 )))
+
+if [ $days_left -lt 8 ]
+then
+	rm server*
+	openssl genrsa -out server.key 2048; #server key generation
+	openssl req -out server.csr -key server.key -new; #certificate signature request generation
+	openssl x509 -req -in server.csr -CA /etc/mosquitto/ca_certificates/ca.crt -CAkey /etc/mosquitto/ca_certificates/ca.key -CAcreateserial -out server.crt -days 45; #certificate signature
+	
+	cd /etc/mosquitto
+	if [[ ! -d cert_log ]]
+	then
+		mkdir cert_log
+	fi
+	cd cert_log
+	sudo echo "[$current_date]:" "(RENEWAL) The server certificate was renewed" >> certificate_renewal.log
+
+	sudo systemctl restart mosquitto
+else
+	cd /etc/mosquitto
+	if [[ ! -d cert_log ]]
+	then
+		mkdir cert_log
+	fi
+	cd cert_log
+	sudo echo "[$current_date]:" "The server certificate was not renewed. "$days_left" days left until its expiration date" >> certificate_renewal.log
+fi
---
+++ server_setup/mqtts_setup.sh
@@ -0,0 +1,66 @@
+#!/bin/bash
+
+echo "$(tput setaf 2)Installing mosquitto and mosquitto-clients $(tput init)"
+sudo apt-get install mosquitto mosquitto-clients
+
+
+while true; do
+    read -p "$(tput setaf 2)Do you have a public domain name for this server? (Y)es/(N)o:$(tput init)" yn
+    case $yn in
+        [Yy]* ) 
+		echo "$(tput setaf 2)Adding certbot repository $(tput init)";
+		sudo add-apt-repository ppa:certbot/certbot;
+
+		echo "$(tput setaf 2)Updating the package list $(tput init)";
+		sudo apt-get update;
+		echo "$(tput setaf 2)Installing certbot $(tput init)";
+		sudo apt-get install certbot;
+
+		echo "$(tput setaf 2)Allowing connections to port 80 before getting your certificates $(tput init)";
+		sudo ufw allow http;
+
+		echo "$(tput setaf 2)Getting your certificate via certbot $(tput init)";
+		sudo certbot certonly --standalone --preferred-challenges http-01;  #--standalone-supported-challenges is deprecated
+
+		sudo crontab -l > cron_temp
+		echo '30 3 * * * certbot renew --noninteractive --post-hook "systemctl restart mosquitto"' >> cron_temp
+		sudo crontab cron_temp
+		sudo rm cron_temp
+
+		#modify mosquitto.conf file here
+		break;;
+        [Nn]* ) 
+		echo "$(tput setaf 2)First create a key pair for the CA $(tput init)";
+		sudo openssl genrsa -des3 -out ca.key 2048
+		echo "$(tput setaf 2)Generating CA certificate and key $(tput init)";
+		sudo openssl req -new -x509 -days 1826 -key ca.key -out ca.crt
+		#openssl req -new -x509 -days 365 -extensions v3_ca -keyout ca.key -out ca.crt; #CA cert + key generation 
+		echo "$(tput setaf 2)Generating server key $(tput init)";
+		sudo openssl genrsa -out server.key 2048; #server key generation
+		echo "$(tput setaf 2)Requesting certificate signature to the CA $(tput init)";
+		sudo openssl req -out server.csr -key server.key -new; #certificate signature request generation
+		echo "$(tput setaf 2)CA signing server certificate $(tput init)";
+		sudo openssl x509 -req -in server.csr -CA ca.crt -CAkey ca.key -CAcreateserial -out server.crt -days 45; #certificate signature by CA
+		echo "$(tput setaf 2)Moving the CA certificate and key to /etc/mosquitto/ca_certificates  $(tput init)";
+		sudo mv ca.crt ca.key /etc/mosquitto/ca_certificates
+		echo "$(tput setaf 2)Moving the server certificate and key to /etc/mosquitto/certs  $(tput init)";
+		sudo mv server.crt server.key /etc/mosquitto/certs
+		#remove csr etc?
+		
+		echo "$(tput setaf 2)Adding new cron job for the automatic server certificate renewal  $(tput init)";
+		sudo crontab -l > cron_temp
+		echo '30 3 * * * ./check_cert' >> cron_temp
+		sudo crontab cron_temp
+		sudo rm cron_temp
+
+		echo "$(tput setaf 2)Updating your mosquitto configuration file  $(tput init)";
+		sudo cat mosquitto.conf > /etc/mosquitto/mosquitto.conf
+		sudo systemctl restart mosquitto.service
+
+		exit;;
+        * ) echo "Please answer yes or no.";;
+    esac
+done
+
+
+
