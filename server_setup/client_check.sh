7c2e5628f749b9f631c1a296536b151896bd29d4
rendezvous <47820025+rendezvous@users.noreply.github.com>
2020-03-22 19:15:21 +0100

    new script to renew client certificates
    works similar to the client setup script, but in this case sends the renewed certs to the client

---
+++ server_setup/client_check.sh
@@ -0,0 +1,73 @@
+#!/bin/bash
+
+
+current_date=$(date)
+current_epoch=$(date -d "$currentdate" +"%s")
+
+host=$(hostname)
+
+		for ((n=0;n<9;n++))
+		do
+			if [[ -d "cli_"$n ]]
+			then
+				cliName="cli_"$n
+				cd $cliName
+				expiration_date=$(openssl x509 -text -noout -in client.crt | grep "Not After")
+				expiration_epoch=$(date -d "${expiration_date//Not After : /}" +"%s")
+
+				seconds_left=$(( $expiration_epoch - $current_epoch ))
+				days_left=$(( $seconds_left / ( 3600 * 24 )))
+				
+				if [ $days_left -lt 5 ]
+				then
+					rm client*
+					echo "$(tput setaf 2)Generating client key $(tput init)";
+					sudo openssl genrsa -out client.key 2048; #server key generation #no expect
+					echo "$(tput setaf 2)Requesting certificate signature to the CA $(tput init)";
+					printf 'ES\n\n\n\n\nclient'$n'\n\n\n\n' | sudo openssl req -out client.csr -key client.key -new; #certificate signature request generation
+					echo "$(tput setaf 2)CA signing server certificate $(tput init)";
+					sudo openssl x509 -req -in client.csr -CA /etc/mosquitto/ca_certificates/ca.crt -CAkey /etc/mosquitto/ca_certificates/ca.key -CAcreateserial -out client.crt --passin pass:1234 -days 45; #certificate signature
+					cd ..
+					echo "$(tput setaf 2)Publishing certificate and key... $(tput init)";
+					certTopic="cl/"$cliName
+					keyTopic="k/"$cliName
+					comTopic="com/"$cliName
+					sudo mosquitto_pub -d -h $host --cafile /etc/mosquitto/ca_certificates/ca.crt -t $certTopic -f $cliName/client.crt -p 8883 --cert $cliName/client.crt --key $cliName/client.key
+			
+   					echo "$cliName certificate published"
+					sudo mosquitto_sub -d -h $host --cafile /etc/mosquitto/ca_certificates/ca.crt -C 1 -t $comTopic -p 8883 --cert $cliName/client.crt --key $cliName/client.key
+			
+					sleep 1
+					sudo mosquitto_pub -d -h $host --cafile /etc/mosquitto/ca_certificates/ca.crt -t $keyTopic -f $cliName/client.key -p 8883 --cert $cliName/client.crt --key $cliName/client.key
+			
+
+					sudo mosquitto_sub -d -h $host --cafile /etc/mosquitto/ca_certificates/ca.crt -C 1 -t $comTopic -p 8883 --cert $cliName/client.crt --key $cliName/client.key
+
+					cd /etc/mosquitto
+					if [[ ! -d cert_log ]]
+					then
+						mkdir client_cert_log
+					fi
+					cd client_cert_log
+					sudo echo "[$current_date]:" "(RENEWAL) Client $n certificate was renewed" >> client_certificate_renewal.log
+
+					sudo systemctl restart mosquitto
+				else
+					cd /etc/mosquitto
+					if [[ ! -d cert_log ]]
+					then
+						mkdir client_cert_log
+					fi
+					cd client_cert_log
+					sudo echo "[$current_date]:" "(RENEWAL) Client $n certificate was renewed" >> client_certificate_renewal.log
+					sudo echo "[$current_date]:" "Client $n certificate was not renewed. "$days_left" days left until its expiration date" >> client_certificate_renewal.log
+				fi
+				
+				
+			else 
+				break;
+
+			fi
+		done
+
+
