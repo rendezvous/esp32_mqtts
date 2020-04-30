398db08489a6f251a110105c58f3b9c67b9bf0e8
rendezvous <47820025+rendezvous@users.noreply.github.com>
2020-04-30 11:12:54 +0200

    added change to clients dir

--- server_setup/restart.sh
+++ server_setup/restart.sh
@@ -2,5 +2,6 @@
 
 sudo systemctl stop mosquitto.service
 sudo cat mosquitto.conf > /etc/mosquitto/mosquitto.conf
+cd /etc/mosquitto/clients
 sudo rm -rf cli_*
 sudo systemctl start mosquitto.service
