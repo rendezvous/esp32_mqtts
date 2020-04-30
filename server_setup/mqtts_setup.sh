0402b3403c68b30093760072f6769fa1d2505c0b
rendezvous <47820025+rendezvous@users.noreply.github.com>
2020-04-30 10:43:18 +0200

    removed unnecessary sudo

--- server_setup/mqtts_setup.sh
+++ server_setup/mqtts_setup.sh
@@ -59,10 +59,10 @@ while true; do
 		sudo cp check_cert.sh /etc/mosquitto/cron
 		sudo chown -R pi:pi /etc/mosquitto/cron
 		echo "$(tput setaf 2)Adding new cron job for the automatic server certificate renewal  $(tput init)";
-		sudo crontab -l > cron_temp
+		crontab -l > cron_temp
 		echo '30 3 * * * /etc/mosquitto/cron/check_cert' >> cron_temp
-		sudo crontab cron_temp
-		sudo rm cron_temp
+		crontab cron_temp
+		rm cron_temp
 
 		echo "$(tput setaf 2)Updating your mosquitto configuration file  $(tput init)";
 		sudo cat mosquitto.conf > /etc/mosquitto/mosquitto.conf
