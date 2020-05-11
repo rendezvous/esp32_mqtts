1d6f851af805dc5a0f35ab06f382599c628edc0b
rendezvous <47820025+rendezvous@users.noreply.github.com>
2020-05-11 17:49:01 +0200

    fixed cp

--- server_setup/client_certificate_setup.sh
+++ server_setup/client_certificate_setup.sh
@@ -48,8 +48,8 @@ while true; do
 			sudo openssl x509 -req -in client.csr -CA /etc/mosquitto/ca_certificates/ca.crt -CAkey /etc/mosquitto/ca_certificates/ca.key -CAcreateserial -out client.crt --passin pass:1234 -days 700
 		cd ..
 		sudo systemctl stop mosquitto.service
-		sudo cp acl_file /etc/mosquitto
-		sudo cat client_conf > /etc/mosquitto/mosquitto.conf
+		sudo cp acl_file /etc/mosquitto/acl_file
+		sudo cp client_conf > /etc/mosquitto/mosquitto.conf
 		sudo echo "require_certificate true" >> /etc/mosquitto/mosquitto.conf
 		sudo echo "use_identity_as_username true" >> /etc/mosquitto/mosquitto.conf
 		
