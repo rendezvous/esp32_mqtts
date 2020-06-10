87b55715e8f2a49cfa87e86436a70863a31b5029
rendezvous <47820025+rendezvous@users.noreply.github.com>
2020-06-10 12:55:05 +0200

    cleaned up parameters and fixed CN

--- server_setup/check_cert.sh
+++ server_setup/check_cert.sh
@@ -10,13 +10,13 @@ current_epoch=$(date -d "$currentdate" +"%s")
 seconds_left=$(( $expiration_epoch - $current_epoch ))
 days_left=$(( $seconds_left / ( 3600 * 24 )))
 
-host=$(hostname)
+
 
 if [ $days_left -lt 8 ]
 then
 	rm server*
 	openssl genrsa -out server.key 2048; #server key generation
-	printf 'ES\n\n\n\n\n'$host'\n\n\n\n' | openssl req -out server.csr -key server.key -new; #certificate signature request generation
+	openssl req -out server.csr -key server.key -new "/C=ES/ST=Palma/L=test/O=Global Security/OU=IT Department/CN=192.168.1.40"; #certificate signature request generation
 	openssl x509 -req -in server.csr -CA /etc/mosquitto/ca_certificates/ca.crt -CAkey /etc/mosquitto/ca_certificates/ca.key -CAcreateserial -out server.crt -passin pass:1234 -days 45; #certificate signature
 	
 	cd /etc/mosquitto/cron
